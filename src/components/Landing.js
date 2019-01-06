6abbb56e898e8a3eecce36e8f5e44137bf6148ba
Thomas Morris <rthomasmorris@gmail.com>
2019-01-06 10:29:18 -0500

    modify

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -2,11 +2,27 @@ import React from 'react';
 
 const Landing = () => (
 
-  <div class="jumbotron text-center">
-  <h1>Turn up the music!</h1>
-  <p>The world is full of music so why not choose your own?</p>
-  <p>No arbitrary limits. No distractions</p>
-  <p>Mobile Enabled</p>
+  <div class="container">
+
+    <div class="jumbotron">
+
+     <h1 class= "text-center">Turn Up The Music!</h1>
+     <img src="https://img3.stockfresh.com/files/b/brux/m/67/5074233_stock-vector-robot-music-lover.jpg" class="rounded mx-auto d-block"/>
+
+  <div class="container">
+  <div class="row">
+  <div class="col-sm text-primary">
+  	<h2>The world is full of music.  Why not choose your own?</h2>
+  </div>
+  <div class="col-sm text-danger">
+  	<h2>No arbitrary limits or distractions.</h2>
+  </div>
+  <div class="col-sm text-warning text-center">
+  	<h2>Mobile enabled!</h2>
+  </div>
+  </div>
+  </div>
+  </div>
   </div>
       
      
--- src/components/Library.js
+++ src/components/Library.js
@@ -12,20 +12,40 @@ class Library extends Component {
 
    render() {
     return ( 
-      <section className='Library'>
-          {
-          this.state.albums.map( (album, index) => 
-             <Link to={`/album/${album.slug}`} key={index}>
-              <img src={album.albumCover} alt={album.title} />
-               <div>{album.title}</div>
-               <div>{album.artist}</div>
-               <div>{album.songs.length} songs</div>
-            </Link>
-          )
-        }
-      </section>
-  );
- }
+    
+      <div className="row">
+        <div className="col-sm-6">
+          <div className="card">
+          
+            {this.state.albums.map((album, index) => (
+              <Link to={`/album/${album.slug}`} key={index}>
+                <img src={album.albumCover} className="card-image-top" />
+                <div className="card-body">
+                  <h5 className="card-title">{album.title}</h5>
+                  <p className="card-text">{album.artist}</p>
+                  <p className="card-text">{album.songs.length} songs</p>
+                </div>
+              </Link>
+            ))}
+          </div>
+        </div>
+        <div className="card">
+          <div className="col-sm-6">
+            {this.state.albums.map((album, index) => (
+              <Link to={`/album/${album.slug}`} key={index}>
+                <img src={album.albumCover} className="card-image-top" />
+                <div className="card-body">
+                  <h5 className="card-title">{album.title}</h5>
+                  <p className="card-text">{album.artist}</p>
+                  <p className="card-text">{album.songs.length} songs</p>
+                </div>
+              </Link>
+            ))}
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Library;
